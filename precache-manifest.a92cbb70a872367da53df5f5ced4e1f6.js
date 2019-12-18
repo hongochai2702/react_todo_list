958998c5829d6ad0faea50408182280a2ae16d1c
hnhai0401 <hongochai0401@gmail.com>
2019-12-18 21:53:41 +0700

    Updates

---
+++ precache-manifest.a92cbb70a872367da53df5f5ced4e1f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89be1b7d180f4bb4bc5c59e457494358",
+    "url": "/react_todo_list/index.html"
+  },
+  {
+    "revision": "3a7c6e65a9920abb4edf",
+    "url": "/react_todo_list/static/css/main.aa3bbf1f.chunk.css"
+  },
+  {
+    "revision": "7baa0e9b913ae9a776ae",
+    "url": "/react_todo_list/static/js/2.d75a7f20.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/react_todo_list/static/js/2.d75a7f20.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3a7c6e65a9920abb4edf",
+    "url": "/react_todo_list/static/js/main.71e803f8.chunk.js"
+  },
+  {
+    "revision": "1f533b50f59a618c575d",
+    "url": "/react_todo_list/static/js/runtime-main.337f7500.js"
+  }
+]);
